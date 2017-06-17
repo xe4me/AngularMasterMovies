7e51fae6d79f488a98d87a0e21bc8ad080afc9e4
Milad <smiladhi@gmail.com>
2017-06-17 22:59:17 +1000

    interpolation

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from "@angular/core";
 
 @Component({
-  selector: 'app-movie-card',
-  templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.css']
+    selector: 'app-movie-card',
+    templateUrl: './movie-card.component.html',
+    styleUrls: ['./movie-card.component.css']
 })
-export class MovieCardComponent implements OnInit {
+export class MovieCardComponent {
+    public movie = {
+        poster_path: 'assets/troy.jpg',
+        release_date: '10/10/1987',
+        vote_average: 980,
+        title: 'Troy'
+    };
 
-  constructor() { }
 
-  ngOnInit() {
-  }
-
+    public update() {
+        this.movie = {
+            poster_path: 'assets/shawshank.jpg',
+            release_date: '10/10/2012',
+            vote_average: 1000,
+            title: 'Shawshank Redemption'
+        }
+    }
 }
