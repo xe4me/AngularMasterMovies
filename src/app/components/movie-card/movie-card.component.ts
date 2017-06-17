436015199d9f5e0908744dd316533a699bf92628
Milad <smiladhi@gmail.com>
2017-06-18 00:05:56 +1000

    @input movies

--- src/app/components/movie-card/movie-card.component.ts
+++ src/app/components/movie-card/movie-card.component.ts
@@ -1,4 +1,4 @@
-import {Component} from "@angular/core";
+import {Component, Input} from "@angular/core";
 
 @Component({
     selector: 'app-movie-card',
@@ -6,10 +6,5 @@ import {Component} from "@angular/core";
     styleUrls: ['./movie-card.component.css']
 })
 export class MovieCardComponent {
-    public movie = {
-        poster_path: 'assets/troy.jpg',
-        release_date: '10/10/1987',
-        vote_average: 980,
-        title: 'Troy'
-    };
+    @Input() movie;
 }
--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -6,7 +6,20 @@ import {Component} from "@angular/core";
     styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent {
-    public movies = [1, 2, 3, 4];
+    public movies = [
+        {
+            poster_path: 'assets/troy.jpg',
+            release_date: '10/10/1987',
+            vote_average: 980,
+            title: 'Troy'
+        },
+        {
+            poster_path: 'assets/shawshank.jpg',
+            release_date: '10/10/2012',
+            vote_average: 2000,
+            title: 'Shaw shank Redemption'
+        }
+    ];
 
     public search($event) {
         console.log('target', $event.value)
