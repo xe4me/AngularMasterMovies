8983a44b4e606e5722883ea986268fe1e0490e6d
Milad <smiladhi@gmail.com>
2017-06-18 00:51:39 +1000

    No Provider

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -7,23 +7,17 @@ import {MovieService} from "../../services/movie.service";
     styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent {
-    public movies = [
-        {
-            poster_path: 'assets/troy.jpg',
-            release_date: '10/10/1987',
-            vote_average: 980,
-            title: 'Troy'
-        },
-        {
-            poster_path: 'assets/shawshank.jpg',
-            release_date: '10/10/2012',
-            vote_average: 2000,
-            title: 'Shaw shank Redemption'
-        }
-    ];
-    public searchService = new MovieService();
+    public popularList;
+
+    constructor(private movieService: MovieService) {
+        this.movieService
+            .getPopular()
+            .subscribe(res => {
+                this.popularList = res.results;
+            });
+    }
 
     public search(query) {
-        this.searchService.searchMovies(query);
+        this.movieService.searchMovies(query);
     }
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,7 +1,23 @@
+import {Http} from "@angular/http";
+import "rxjs/Rx";
+
 export class MovieService {
+    apikey: string = 'fed69657ba4cc6e1078d2a6a95f51c8c';
+    private apiBaseUrl: string = 'https://api.themoviedb.org/3/';
+
+    constructor(private http: Http) {
+    }
 
     searchMovies(searchStr: string) {
         console.log('MovieServiceService: ', searchStr);
     }
 
+    getPopular() {
+        const search = new URLSearchParams();
+        search.set('sort_by', 'popularity.desc');
+        search.set('api_key', this.apikey);
+        return this.http
+            .get(`${this.apiBaseUrl}discover/movie`, {search})
+            .map(res => res.json())
+    }
 }
