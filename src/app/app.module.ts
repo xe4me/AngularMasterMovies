b2a1ee1d60e4b822c2702f2f6e91756707c00385
Milad <smiladhi@gmail.com>
2017-06-17 18:32:17 +1000

    movie card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './components/movies/movies.component';
+import { MovieCardComponent } from './components/movie-card/movie-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesComponent
+    MoviesComponent,
+    MovieCardComponent
   ],
   imports: [
     BrowserModule
