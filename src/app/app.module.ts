c8062fbdf711029e70d325d6e204a0ecf9136ce0
Milad <smiladhi@gmail.com>
2017-06-19 22:43:21 +1000

    using published my-components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {MoviesComponent} from './components/movies/movies.component';
 import {MovieCardComponent} from './components/movie-card/movie-card.component';
-import {SearchBarComponent} from './components/search-bar/search-bar.component';
+import {SearchBarComponent} from 'my-components';
 import {MovieService} from "app/services/movie.service";
 import {HttpModule} from "@angular/http";
 import {LeftNavComponent} from './components/left-nav/left-nav.component';
